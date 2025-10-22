7e5b7dd3442d2a679e98134f43a19581513dcb6d
Rizzna614 <robertsrazna6@gmail.com>
2025-10-22 14:10:04 +0300

    varetu pabeigt majas

---
+++ problemas-risinajums(uzdevums3).js
@@ -0,0 +1,13 @@
+let saraksts = {
+    1: {nosaukums: "Veļasmašīna", kategorija: "Elektronika", cena: 300, noliktavas_daudzums: 3},
+    2: {nosaukums: "Mazais Princis", kategorija: "Grāmata", cena: 30, noliktavas_daudzums: 6},
+    3: {nosaukums: "Ābols", kategorija: "Ēdiens", cena: 1, noliktavas_daudzums: 41},
+    4: {nosaukums: "Krekls", kategorija: "Apģērbs", cena: 20, noliktavas_daudzums: 7},
+    5: {nosaukums: "Telefons", kategorija: "Elektronika", cena: 500, noliktavas_daudzums: 5},
+    6: {nosaukums: "Dators", kategorija: "Elektronika", cena: 800, noliktavas_daudzums: 2},
+    7: {nosaukums: "Bikses", kategorija: "Apģērbs", cena: 25, noliktavas_daudzums: 8},
+    8: {nosaukums: "Cīņa pret cukuru", kategorija: "Grāmata", cena: 15, noliktavas_daudzums: 1},
+    };
+
+    
+
